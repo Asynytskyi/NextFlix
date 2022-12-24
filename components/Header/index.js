759bebfd2334dbd1eb0c22540a0c9cf5f97df47f
Artem Synytskyi <si.artem05@gmail.com>
2022-12-24 01:51:47 +0100

    Header_Update

---
+++ components/Header/index.js
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react";
+
+import Logo from "../Logo";
+import Hamburger from "../IconHamburger";
+import Close from "../IconClose";
+import MenuComponents from "../MenuComponents";
+import SearchBar from "../SearchBar";
+
+const Header = ({ requireMovies }) => {
+  const [movies, setMovies] = useState([]);
+  const [active, setActive] = useState(false);
+
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [defaultScrollY, setDefaultScrollY] = useState(80);
+
+  const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=a8fac808";
+
+  async function searchMovies(title) {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      setMovies(data.Search);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Default onfirstload search
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  useEffect(() => {
+    requireMovies(movies);
+  }, [movies]);
+
+  const controlHeader = () => {
+    active ? setDefaultScrollY(9999) : setDefaultScrollY(80);
+
+    if (typeof window !== "undefined") {
+      window.scrollY > lastScrollY && window.scrollY > defaultScrollY
+        ? // if scroll down hide the navbar
+          setShow(false)
+        : // if scroll up show the navbar
+          setShow(true);
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlHeader);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlHeader);
+      };
+    }
+  }, [lastScrollY]);
+
+  return (
+    <header
+      className={`${
+        show
+          ? "border-b-2 border-[var(--bg)] backdrop-blur-xl fixed top-0 right-0 left-0 z-20 w-full h-20 flex flex-row items-center justify-between px-10"
+          : "hidden"
+      }`}
+    >
+      <Logo />
+      <div className="w-2/3 opacity-60">
+        <SearchBar
+          movies={movies}
+          onChange={(name) => {
+            searchMovies(name);
+          }}
+        />
+      </div>
+      <button
+        className="flex items-center justify-center w-6.4375"
+        onClick={() => setActive(!active)}
+      >
+        {active ? <Close /> : <Hamburger />}
+      </button>
+      <nav>{active ? <MenuComponents /> : <></>}</nav>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,18 @@
 import { useState, useEffect, use } from "react";
 
 import Card from "../components/Card";
-import SearchBar from "../components/SearchBar";
-import Logo from "../components/Logo";
-import Hamburger from "../components/IconHamburger";
-import Close from "../components/IconClose";
-import MenuComponents from "../components/MenuComponents";
-import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 export default function Home() {
-  const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=a8fac808";
-
-  const [movies, setMovies] = useState([]);
-  const [active, setActive] = useState(false);
-
-  async function searchMovies(title) {
-    try {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-      setMovies(data.Search);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // Default onfirstload search
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
+  const [allMovies, setAllMovies] = useState([]);
 
   return (
     <div className="mx-auto flex flex-col items-center gap-y-20 pb-40">
-      <Navbar>
-        <Logo />
-        <div className="w-2/3 opacity-60">
-          <SearchBar
-            movies={movies}
-            onChange={(name) => {
-              searchMovies(name);
-            }}
-          />
-        </div>
-        <button
-          className="flex items-center justify-center w-6.4375"
-          onClick={() => setActive(!active)}
-        >
-          {active ? <Close /> : <Hamburger />}
-        </button>
-      </Navbar>
-      <nav>{active ? <MenuComponents /> : <></>}</nav>
-      <div>
-        {movies?.length > 0 ? (
+      <Header requireMovies={setAllMovies} />
+      <div className="pt-44">
+        {allMovies?.length > 0 ? (
           <main className="container flex flex-wrap justify-center gap-x-12 gap-y-14">
-            {movies.map((movie, index) => {
+            {allMovies.map((movie, index) => {
               return (
                 <Card
                   title={movie.Title}
